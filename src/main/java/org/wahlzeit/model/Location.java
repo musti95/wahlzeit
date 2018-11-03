b83496a9500b06fd221afec58588652dc880d402
Dominik Prossel <dominik.prossel@fau.de>
2018-11-03 15:29:57 +0100

    make fields private

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -4,11 +4,27 @@ package org.wahlzeit.model;
  * A location represents the place, where a {@link Photo} has been taken.
  */
 public class Location {
-	public Coordinate coordinate;
-	public String name;
+	private Coordinate coordinate;
+	private String name;
 
 	Location(Coordinate coordinate, String name) {
 		this.coordinate = coordinate;
+		this.name = name;
+	}
+
+	public Coordinate getCoordinate() {
+		return coordinate;
+	}
+
+	public void setCoordinate(Coordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
 }
