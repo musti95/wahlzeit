102c0b88c93d029f38749e7c613114c0d4d9aa5d
Dominik Prossel <dominik.prossel@fau.de>
2018-11-12 10:36:00 +0100

    add GuitaristPhoto class

---
+++ src/main/java/org/wahlzeit/model/GuitaristPhoto.java
@@ -0,0 +1,36 @@
+package org.wahlzeit.model;
+
+public class GuitaristPhoto extends Photo {
+    private String guitaristName = "";
+    private String guitar = "";
+
+    public GuitaristPhoto() {
+        super();
+    }
+
+    public GuitaristPhoto(PhotoId myId) {
+        super(myId);
+    }
+
+    public GuitaristPhoto(PhotoId myId, String name, String guitar){
+        super(myId);
+        this.guitaristName = name;
+        this.guitar = guitar;
+    }
+
+    public String getGuitaristName() {
+        return guitaristName;
+    }
+
+    public void setGuitaristName(String guitaristName) {
+        this.guitaristName = guitaristName;
+    }
+
+    public String getGuitar() {
+        return guitar;
+    }
+
+    public void setGuitar(String guitar) {
+        this.guitar = guitar;
+    }
+}
