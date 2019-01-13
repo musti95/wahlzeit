efa5bf75ab66c56e117e4fdcefb0eab7fcff7c92
Dominik Prossel <dominik.prossel@fau.de>
2019-01-13 21:16:22 +0100

    Add PatternInstance Annotation

--- src/main/java/org/wahlzeit/model/Photo.java
+++ src/main/java/org/wahlzeit/model/Photo.java
@@ -31,12 +31,18 @@ import org.wahlzeit.services.DataObject;
 import org.wahlzeit.services.EmailAddress;
 import org.wahlzeit.services.Language;
 import org.wahlzeit.services.ObjectManager;
+import org.wahlzeit.utils.PatternInstance;
 
 import java.util.Map;
 
 /**
  * A photo represents a user-provided (uploaded) photo.
  */
+@PatternInstance(
+		patternName = "Abstract Factory",
+		participants = {
+				"AbstractProduct", "ConcreteProduct"
+		})
 @Entity
 public class Photo extends DataObject {
 
--- src/main/java/org/wahlzeit/model/PhotoFactory.java
+++ src/main/java/org/wahlzeit/model/PhotoFactory.java
@@ -21,12 +21,18 @@
 package org.wahlzeit.model;
 
 import org.wahlzeit.services.LogBuilder;
+import org.wahlzeit.utils.PatternInstance;
 
 import java.util.logging.Logger;
 
 /**
  * An Abstract Factory for creating photos and related objects.
  */
+@PatternInstance(
+		patternName = "Abstract factory",
+		participants = {
+				"AbstractFactory", "ConcreteFactory"
+		})
 public class PhotoFactory {
 
 	private static final Logger log = Logger.getLogger(PhotoFactory.class.getName());
